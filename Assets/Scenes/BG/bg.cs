0dd1e20ba92b40fef47d3799e5aebab828343241
paremai15149-art <paremai15149@gmail.com>
2026-05-01 22:41:15 +0700

    UI credit

---
+++ Assets/Scenes/BG/bg.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BGLoop : MonoBehaviour
+{
+    public float speed = 200f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.localPosition.x <= -1920) // ขนาดจอ
+        {
+            transform.localPosition += new Vector3(1920 * 2, 0, 0);
+        }
+    }
+}
