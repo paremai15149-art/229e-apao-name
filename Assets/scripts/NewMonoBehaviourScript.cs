9aa1a8cfca94d97ee7e5e611347f08fe979ed0a5
paremai15149-art <paremai15149@gmail.com>
2026-05-02 00:07:33 +0700

    link ui

---
+++ Assets/scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public void LoadCredit()
+    {
+        SceneManager.LoadScene("credit");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("maingame"); // หรือชื่อ scene เกมจริง
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("EXIT CLICKED");
+
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false; // ?? หยุด Play Mode
+#else
+        Application.Quit(); // ?? ออกเกมจริง
+#endif
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("CLICK MENU");
+        SceneManager.LoadScene("UI_Menu");
+    }
+
+}
